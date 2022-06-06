bbf8a2e0d52e23dedd28f04b06c9ae09b54988eb
Wilmer Garzon <garzonwilmer5@gmail.com>
2022-06-06 13:46:34 +0000

    [FEAT]: Windup th fix

--- robotics/ros2/src/motion_control/src/pid_controller.cpp
+++ robotics/ros2/src/motion_control/src/pid_controller.cpp
@@ -37,16 +37,15 @@ float PIDController::ThrottlePID(float ref_vx, float cur_vx, double dt)
                               + m_kd_thr * e_diff;
 
     // Anti-Windup: Back-calculation
-    float signal_vx_max = 60.0f * m_max_linear_spd / (2 * PI * m_wheel_rad);
     float ep = 0;
     // saturated signal
-    if(signal_vx_max < std::fabs(controller_signal))
+    if(m_max_linear_spd < std::fabs(cur_vx))
     {
-        ep = controller_signal > 0 ? 
-                signal_vx_max - controller_signal : 
-                -signal_vx_max - controller_signal;
+        ep = cur_vx > 0 ? 
+                m_max_linear_spd - cur_vx : 
+                -m_max_linear_spd - cur_vx;
     }         
-    m_vx_int_error += (m_ki_thr * error + ep / m_tt_thr) *dt;
+    m_vx_int_error += (m_ki_thr * error + ep / m_tt_thr) * dt;
 
     m_vx_prop_ek1 = error;
     m_prev_ref_vx = ref_vx;
@@ -83,16 +82,13 @@ float PIDController::SteeringPID(float ref_wz, float cur_wz, double dt)
     controller_signal += m_kff_str * ref_wz;
 
     // Anti-Windup: Back-calculation
-    float signal_wz_max = m_max_angular_spd * m_robot_track / 2.0f 
-                          * 60.0f / (2 * PI * m_wheel_rad);
-
     float ep = 0;
     // saturated signal
-    if(signal_wz_max < std::fabs(controller_signal))
+    if(m_max_angular_spd < std::fabs(cur_wz))
     {
-        ep = controller_signal > 0 ? 
-                signal_wz_max - controller_signal : 
-                -signal_wz_max - controller_signal;
+        ep = cur_wz > 0 ? 
+                m_max_angular_spd - cur_wz : 
+                -m_max_angular_spd - cur_wz;
     }
     m_wz_int_error += (m_ki_str * error + ep / m_tt_str) *dt;
     
